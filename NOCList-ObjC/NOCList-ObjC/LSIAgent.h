c8742229bb3bff1218190b3a4105021c65d0ed18
Kerby Jean <kerby.jean@hotmail.fr>
2020-04-23 21:24:23 -0400

    Added LSIAgent class

---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.h
@@ -0,0 +1,22 @@
+//
+//  LSIAgent.h
+//  NOCList-ObjC
+//
+//  Created by Kerby Jean on 4/23/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIAgent : NSObject
+
+@property NSString *coverName;
+@property NSString *realName;
+@property NSInteger *accessLevel;
+@property Boolean *compromised;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NOCList-ObjC/NOCList-ObjC/LSIAgent.m
@@ -0,0 +1,19 @@
+//
+//  LSIAgent.m
+//  NOCList-ObjC
+//
+//  Created by Kerby Jean on 4/23/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import "LSIAgent.h"
+
+@implementation LSIAgent
+
+
+
+
+
+
+
+@end
