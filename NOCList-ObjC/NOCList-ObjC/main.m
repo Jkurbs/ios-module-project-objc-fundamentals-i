c15e98f268f5929a0072b1ed0e26d0efb8c21307
Kerby Jean <kerby.jean@hotmail.fr>
2020-04-23 21:19:31 -0400

    First commit -m

---
+++ NOCList-ObjC/NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Kerby Jean on 4/23/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
